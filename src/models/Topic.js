0fe0cc264f54e6a0380fb10b1ff5604b047a3996
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-08 18:27:06 -0300

    fix: update Topic Model and relationship with chapter

---
+++ src/models/Topic.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../utils/database');
+
+class Topic extends Sequelize.Model {}
+
+Topic.init(
+  {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize,
+    modelName: 'topic',
+  },
+);
+
+module.exports = Topic;
--- src/utils/loadRelationships.js
+++ src/utils/loadRelationships.js
@@ -2,7 +2,9 @@ const Course = require('../models/Course');
 const CourseUser = require('../models/CourseUser');
 const User = require('../models/User');
 const Chapter = require('../models/Chapter');
+const Topic = require('../models/Topic');
 
 Course.belongsToMany(User, { through: CourseUser });
 User.belongsToMany(Course, { through: CourseUser });
 Course.hasMany(Chapter);
+Chapter.hasMany(Topic);
