39b670a461fdd8513dfde47c77695ff46c917dc4
Bruna <brutopanb@gmail.com>
2021-02-17 19:58:58 -0300

    fix: adding dialectOptions

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,10 @@ const { Sequelize } = require('sequelize');
 
 const options = { logging: false, dialect: "postgres" };
 
+if (process.env.NODE_ENV === "production") {
+  options.dialectOptions = { ssl: { rejectUnauthorized: false } };
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, options);
 
 module.exports = sequelize;
