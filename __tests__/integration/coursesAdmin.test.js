42b07931c223c1d2900f17f002b99aadbab54d1a
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-18 19:32:22 -0300

    feat: getAll  route course as Admin

--- __tests__/integration/coursesAdmin.test.js
+++ __tests__/integration/coursesAdmin.test.js
@@ -230,3 +230,34 @@ describe('GET /admin/courses/:id', () => {
     expect(response.status).toBe(403);
   });
 });
+
+describe('GET /admin/courses/', () => {
+  it('should return 200', async () => {
+    const course = {
+      name: 'JavaScript21122',
+      image: 'https://static.imasters.com.br/wp-content/uploads/2018/12/10164438/javascript.jpg',
+      description: 'JavaScript do Zero',
+    };
+    const chapter = { name: 'Apresentação Programação' };
+
+    const resultCourse = await db.query('INSERT INTO courses (name, image, description, "createdAt", "updatedAt") values ($1, $2, $3, $4, $5) RETURNING *', [course.name, course.image, course.description, NOW, NOW]);
+    const courseId = resultCourse.rows[0].id;
+
+    const resultChapter = await db.query('INSERT INTO chapters (name, "courseId", "createdAt", "updatedAt") values ($1, $2, $3, $4) RETURNING *', [chapter.name, courseId, NOW, NOW]);
+    const chapterId = resultChapter.rows[0].id;
+
+    const response = await agent.get('/admin/courses').set({ 'X-Access-Token': tokenAdmin });
+
+    expect(response.status).toBe(200);
+    expect(response.headers['access-control-expose-headers']).toBeTruthy();
+    expect(response.headers['content-range']).toBeTruthy();
+    expect.arrayContaining([{
+      id: courseId,
+      name: course.name,
+      deleted: false,
+      image: course.image,
+      description: course.description,
+      chapters: [chapterId],
+    }]);
+  });
+});
