4dc27e6297bb4f5a42c5866de678246d6a231f22
Bruna <brutopanb@gmail.com>
2021-02-18 16:24:00 -0300

    feat: created theory table

--- src/utils/loadRelationships.js
+++ src/utils/loadRelationships.js
@@ -3,9 +3,12 @@ const CourseUser = require('../models/CourseUser');
 const User = require('../models/User');
 const Chapter = require('../models/Chapter');
 const Topic = require('../models/Topic');
+const Theory = require('../models/Theory');
 
 Course.belongsToMany(User, { through: CourseUser });
 User.belongsToMany(Course, { through: CourseUser });
 Course.hasMany(Chapter);
 Chapter.hasMany(Topic, { onDelete: 'CASCADE' });
 Topic.belongsTo(Chapter, { onDelete: 'CASCADE'});
+Topic.hasMany(Theory, { onDelete: 'CASCADE' });
+Theory.belongsTo(Topic, { onDelete: 'CASCADE' });
