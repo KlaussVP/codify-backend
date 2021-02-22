875979f2cf0db4b5b50bcf20230d4052420f8eb5
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-22 18:00:58 -0300

    feat: test route POST topic

--- src/routers/admin/topicsRouter.js
+++ src/routers/admin/topicsRouter.js
@@ -2,15 +2,22 @@ const topicRouter = require('express').Router();
 const { adminVerifyJWT } = require('../../middlewares/adminMiddlewares');
 
 const topicsController = require('../../controllers/topicsController');
+const { postTopicSchema, editTopicSchema } = require('../../schemas/topicsSchema');
 
 topicRouter.post('/', adminVerifyJWT, async (req, res) => {
+  const validation = postTopicSchema.validate(req.body);
+  if (validation.error) return res.status(422).send({ error: 'Verifique seus dados' });
+
   const topic = await topicsController.createTopic(req.body);
-  res.send(topic);
+  return res.status(201).send(topic);
 });
 
 topicRouter.put('/:id', adminVerifyJWT, async (req, res) => {
+  const validation = editTopicSchema.validate(req.body);
+  if (validation.error) return res.status(422).send({ error: 'Verifique seus dados' });
+
   const topic = await topicsController.editTopic(req.params.id, req.body);
-  res.send(topic);
+  return res.send(topic);
 });
 
 topicRouter.get('/', async (req, res) => {
