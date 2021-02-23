5986ece3e829a76f108f9035cbde3bf4e877ef99
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-23 16:30:28 -0300

    feat: insert and get sessions through Redis in login and auth middleware

---
+++ src/repositories/sessionStore.js
@@ -0,0 +1,37 @@
+const redis = require('promise-redis')();
+
+let client;
+
+function createInstance() {
+  client = redis.createClient({
+    url: process.env.REDISCLOUD_URL,
+  });
+
+  client.on('error', (error) => {
+    /* eslint-disable-next-line no-console */
+    console.error(error);
+  });
+  return client;
+}
+
+function getInstance() {
+  if (!client) client = createInstance();
+  return client;
+}
+
+async function setSession(key, value) {
+  client = getInstance();
+  await client.set(key, JSON.stringify(value), 'EX', process.env.SESSION_EXPIRATION);
+  return key;
+}
+
+async function getSession(key) {
+  client = getInstance();
+  const result = await client.get(key);
+  if (result) {
+    return JSON.parse(result);
+  }
+  return false;
+}
+
+module.exports = { setSession, getSession };
