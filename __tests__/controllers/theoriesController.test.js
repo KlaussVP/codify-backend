b4cf058aa9b945849f3147b10ea2220705faf587
Bruna <brutopanb@gmail.com>
2021-02-25 00:28:29 -0300

    feat: unit tests to theoriesController

--- __tests__/controllers/theoriesController.test.js
+++ __tests__/controllers/theoriesController.test.js
@@ -23,3 +23,54 @@ describe('postTheoryUser', () => {
     }).rejects.toThrow(InexistingId);
   });
 });
+
+describe('getAllTheories', () => {
+  it('should return an array', async () => {
+    const expectedArray = [{ id: 1, youtubeLink: 'https://www.youtube.com/watch?v=08X9gf3mdKY' }];
+    Theory.findAll.mockResolvedValue(expectedArray);
+    const theories = await theoriesController.getAllTheories();
+    expect(theories.length).toBe(1);
+    expect(theories).toEqual(expectedArray);
+  });
+});
+
+describe('getTheoryByIdAsAdmin', () => {
+  it('should return an object', async () => {
+    const id = 1;
+    const youtubeLink = 'https://www.youtube.com/watch?v=08X9gf3mdKY';
+    const expectedObject = { id, youtubeLink };
+    Theory.findByPk.mockResolvedValue(expectedObject);
+    const theory = await theoriesController.getTheoryByIdAsAdmin(id);
+    expect(theory).toEqual(expectedObject);
+  });
+
+  it('should throw an error', async () => {
+    Theory.findByPk.mockResolvedValue(null);
+    expect(async () => {
+      await theoriesController.getTheoryByIdAsAdmin(-1);
+    }).rejects.toThrow(InexistingId);
+  });
+});
+
+describe('editTheory', () => {
+  it('should return an object', async () => {
+    const oldTheoryMocked = {
+      id: 1,
+      youtubeLink: 'https://www.youtube.com/watch?v=08X9gf3mdKY',
+      save: () => {},
+    };
+    const expectedObject = {
+      id: oldTheoryMocked.id,
+      youtubeLink: 'https://www.youtube.com/watch?v=nQ1XX0GrBEs',
+    };
+
+    Theory.findByPk.mockResolvedValue(oldTheoryMocked);
+    const theory = await theoriesController
+      .editTheory(oldTheoryMocked.id, expectedObject.youtubeLink);
+    expect(theory).toEqual(expect.objectContaining({
+      id: expectedObject.id,
+      youtubeLink: expectedObject.youtubeLink,
+      save: expect.any(Function),
+    }));
+  });
+});
